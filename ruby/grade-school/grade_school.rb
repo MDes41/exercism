64f6e36e488fecdadf573d12bf2eab3b47aacba5
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-05-03 16:31:55 -0600

    grade school refactoring

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -3,28 +3,24 @@ class School
   VERSION = 1
 
   def initialize
-    @enrollment = Hash.new
+    @enrollment = Hash.new([])
   end
 
   def to_h
-    @enrollment.map do |pair|
-       [ pair[0], pair[1].sort ]
+    @enrollment.map do |k, v|
+       [ k , v.sort ]
     end.sort.to_h
   end
 
   def add(student_name, student_grade)
-    if @enrollment[student_grade]
-      @enrollment[student_grade] << student_name
-    else
+    if @enrollment[student_grade] == []
       @enrollment[student_grade] = [student_name]
+    else
+      @enrollment[student_grade] << student_name
     end
   end
 
   def grade(level)
-    if @enrollment.keys.include?(level)
-      @enrollment[level].sort
-    else
-      []
-    end
+    @enrollment[level].sort
   end
 end
