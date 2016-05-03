c5cc126fd6396cb7c6e9beaff300ae234187ba25
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-05-03 15:02:33 -0600

    sum of multiples refactor

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -5,7 +5,7 @@ class SumOfMultiples
   end
 
   def to(max_number)
-    result = (0..(max_number - 1)).to_a.find_all do |number|
+    result = (1..(max_number - 1)).to_a.find_all do |number|
       multiples_of(number)
     end
     return 0 if result == []
@@ -14,7 +14,6 @@ class SumOfMultiples
 
   def multiples_of(number)
     @multiples.any? do |multiple|
-      return false if number == 0
       number % multiple == 0
     end
   end
