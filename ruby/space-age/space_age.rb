6530f411b109a70f7991de825574638c8c16c727
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-06-30 23:27:15 -0600

    not working for all

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -2,6 +2,16 @@ class SpaceAge
 
   def initialize(seconds)
     @seconds = seconds
+    @op = {
+    Earth: 31557600,
+    Mercury: 0.2408467,
+    Venus: 0.61519726,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune: 164.79132
+  }
   end
 
   def seconds
@@ -9,18 +19,41 @@ class SpaceAge
   end
 
   def on_earth
-    @earth = @seconds / 31557600.00
-    result = @earth
-    result.round(2)
+    @seconds / @op[:Earth]
   end
 
   def on_mercury
-    result = @seconds / ( 31557600.00 * 0.2408467 )
+    result = @seconds / ( @op[:Earth] * @op[:Mercury] )
     result.round(2)
   end
 
   def on_venus
-    result = @seconds / ( 31557600.00 * 0.61519726 )
+    result = @seconds / ( @op[:Earth] * @op[:Venus] )
+    result.round(2)
+  end
+
+  def on_mars
+    result = @seconds / ( @op[:Earth] * @op[:Mars] )
+    result.round(2)
+  end
+
+  def on_jupiter
+    result = @seconds / ( @op[:Earth] * @op[:Jupiter] )
+    result.round(2)
+  end
+
+  def on_saturn
+    result = @seconds / ( @op[:Earth] * @op[:Saturn] )
+    result.round(2)
+  end
+
+  def on_uranus
+    result = @seconds / ( @op[:Earth] * @op[:Uranus] )
+    result.round(2)
+  end
+
+  def on_neptune
+    result = @seconds / ( @op[:Earth] * @op[:Neptune] )
     result.round(2)
   end
 
