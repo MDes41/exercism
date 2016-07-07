d243ba654defc91cc6bc827c1e338a604e7a9bbe
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-07-06 19:01:52 -0600

    binary search tree

---
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -0,0 +1,19 @@
+class Bst
+
+  def initialize(number)
+    @number = number
+  end
+
+  def data
+    @number
+  end
+
+  def insert(number)
+    @number += number
+  end
+
+  def left
+
+  end
+
+end
