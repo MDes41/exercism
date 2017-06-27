00b93b8c08082c916a546020538d36fb183d0ee2
Matt D <MDes41@users.noreply.github.com>
2017-06-26 22:02:11 -0600

    Can not get to_s working without instansiating new

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,8 +1,20 @@
 require 'time'
 class Clock
-  def self.at(hour, minute)
-    time = Time.strptime("#{hour}:#{minute}", "%I:%M")
-    require "pry"; binding.pry
-    "#{"%.2d" % time.hour}:#{"%.2d" % time.min}"
+
+  def self.to_s
+    # self.new
+    # time = Time.strptime("#{self / 60}:#{self % 60}", "%H:%M")
+    # "#{"%.2d" % time.hour}:#{"%.2d" % time.min}"
+    # 'name'
   end
+
+  def self.at(hour, minute)
+    hours_of_minutes = minute / 60
+    hour = (hours_of_minutes + hour) % 24
+    minute = minute % 60
+    @total_minutes = (hour * 60) + minute
+    @total_minutes
+  end
+
+
 end
