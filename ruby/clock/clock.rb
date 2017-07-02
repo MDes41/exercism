362d11c9f1f49ade41e4c522b3f5410ec304a0dd
Matt D <MDes41@users.noreply.github.com>
2017-07-01 22:37:22 -0600

    Tests completed

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,16 +1,46 @@
+class BookKeeping
+  VERSION = 2
+end
+
 class Clock
-  attr_reader :total_minutes
 
   def initialize(hour, minute)
-    @total_minutes = (hour * 60) + minute
+    @total_minutes = TotalMinutes.new(hour, minute)
   end
 
   def to_s
-    "#{"%.2d" % ((@total_minutes / 60) % 24)}:#{"%.2d" % (@total_minutes % 60)}"
+    total_minutes.displayed
   end
 
   def self.at(hour, minute)
     new(hour, minute)
   end
 
+  def +(minute) 
+    total_minutes.add(minute)
+    self
+  end
+
+  def ==(compare)
+    to_s == compare.to_s
+  end
+
+  private
+    attr_reader :total_minutes
+end
+
+class TotalMinutes
+  attr_reader :total_minutes
+
+  def initialize(hour, minute)
+    @total_minutes = (hour * 60) + minute
+  end
+
+  def displayed
+    "#{"%.2d" % ((total_minutes / 60) % 24)}:#{"%.2d" % (total_minutes % 60)}"
+  end
+
+  def add(minute)
+    @total_minutes += minute
+  end
 end
--- ruby/clock/clock_test.rb
+++ ruby/clock/clock_test.rb
@@ -108,180 +108,181 @@ class ClockTest < Minitest::Test
   end
 
   def test_add_no_minutes
-    skip
+    # skip
     assert_equal '06:41', (Clock.at(6, 41) + 0).to_s
   end
 
   def test_add_to_next_hour
-    skip
+    # skip
     assert_equal '01:25', (Clock.at(0, 45) + 40).to_s
   end
 
   def test_add_more_than_one_hour
-    skip
+    # skip
     assert_equal '11:01', (Clock.at(10, 0) + 61).to_s
   end
 
   def test_add_more_than_two_hours_with_carry
-    skip
+    # skip
     assert_equal '03:25', (Clock.at(0, 45) + 160).to_s
   end
 
   def test_add_across_midnight
-    skip
+    # skip
     assert_equal '00:01', (Clock.at(23, 59) + 2).to_s
   end
 
   def test_add_more_than_one_day__1500_min_is_equal_to_25_hrs
-    skip
+    # skip
     assert_equal '06:32', (Clock.at(5, 32) + 1500).to_s
   end
 
   def test_add_more_than_two_days
-    skip
+    # skip
     assert_equal '11:21', (Clock.at(1, 1) + 3500).to_s
   end
 
   def test_subtract_minutes
-    skip
+    # skip
     assert_equal '10:00', (Clock.at(10, 3) + -3).to_s
   end
 
   def test_subtract_to_previous_hour
-    skip
+    # skip
     assert_equal '09:33', (Clock.at(10, 3) + -30).to_s
   end
 
   def test_subtract_more_than_an_hour
-    skip
+    # skip
     assert_equal '08:53', (Clock.at(10, 3) + -70).to_s
   end
 
   def test_subtract_across_midnight
-    skip
+    # skip
     assert_equal '23:59', (Clock.at(0, 3) + -4).to_s
   end
 
   def test_subtract_more_than_two_hours
-    skip
+    # skip
     assert_equal '21:20', (Clock.at(0, 0) + -160).to_s
   end
 
   def test_subtract_more_than_two_hours_with_borrow
-    skip
+    # skip
     assert_equal '03:35', (Clock.at(6, 15) + -160).to_s
   end
 
   def test_subtract_more_than_one_day__1500_min_is_equal_to_25_hrs
-    skip
+    # skip
     assert_equal '04:32', (Clock.at(5, 32) + -1500).to_s
   end
 
   def test_subtract_more_than_two_days
-    skip
+    # skip
     assert_equal '00:20', (Clock.at(2, 20) + -3000).to_s
   end
 
   def test_clocks_with_same_time
-    skip
+    # skip
+    
     clock1 = Clock.at(15, 37)
     clock2 = Clock.at(15, 37)
     assert clock1 == clock2
   end
 
   def test_clocks_a_minute_apart
-    skip
+    # skip
     clock1 = Clock.at(15, 36)
     clock2 = Clock.at(15, 37)
     refute clock1 == clock2
   end
 
   def test_clocks_an_hour_apart
-    skip
+    # skip
     clock1 = Clock.at(14, 37)
     clock2 = Clock.at(15, 37)
     refute clock1 == clock2
   end
 
   def test_clocks_with_hour_overflow
-    skip
+    # skip
     clock1 = Clock.at(10, 37)
     clock2 = Clock.at(34, 37)
     assert clock1 == clock2
   end
 
   def test_clocks_with_hour_overflow_by_several_days
-    skip
+    # skip
     clock1 = Clock.at(3, 11)
     clock2 = Clock.at(99, 11)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_hour
-    skip
+    # skip
     clock1 = Clock.at(22, 40)
     clock2 = Clock.at(-2, 40)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_hour_that_wraps
-    skip
+    # skip
     clock1 = Clock.at(17, 3)
     clock2 = Clock.at(-31, 3)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_hour_that_wraps_multiple_times
-    skip
+    # skip
     clock1 = Clock.at(13, 49)
     clock2 = Clock.at(-83, 49)
     assert clock1 == clock2
   end
 
   def test_clocks_with_minute_overflow
-    skip
+    # skip
     clock1 = Clock.at(0, 1)
     clock2 = Clock.at(0, 1441)
     assert clock1 == clock2
   end
 
   def test_clocks_with_minute_overflow_by_several_days
-    skip
+    # skip
     clock1 = Clock.at(2, 2)
     clock2 = Clock.at(2, 4322)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_minute
-    skip
+    # skip
     clock1 = Clock.at(2, 40)
     clock2 = Clock.at(3, -20)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_minute_that_wraps
-    skip
+    # skip
     clock1 = Clock.at(4, 10)
     clock2 = Clock.at(5, -1490)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_minute_that_wraps_multiple_times
-    skip
+    # skip
     clock1 = Clock.at(6, 15)
     clock2 = Clock.at(6, -4305)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_hours_and_minutes
-    skip
+    # skip
     clock1 = Clock.at(7, 32)
     clock2 = Clock.at(-12, -268)
     assert clock1 == clock2
   end
 
   def test_clocks_with_negative_hours_and_minutes_that_wrap
-    skip
+    # skip
     clock1 = Clock.at(18, 7)
     clock2 = Clock.at(-54, -11_513)
     assert clock1 == clock2
@@ -304,7 +305,7 @@ class ClockTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    # skip
     assert_equal 2, BookKeeping::VERSION
   end
 end
