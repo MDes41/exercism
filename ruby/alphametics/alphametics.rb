99d542188be3327bce9b90ad56980c66255c7e0c
Matt D <MDes41@users.noreply.github.com>
2017-07-05 09:15:58 -0600

    Need to rework from the beginning

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,20 +1,65 @@
 class Alphametics
+  attr_reader :answer
+
   def initialize(input)
-    @input = input.chars
+    @valuation = get_result(input)
+    @arguments = get_arguments(input)
+    @answer = Solve.new([@arguments, @valuation])
   end
 
   def self.solve(input)
-    get_result(input)
-    get_arguments(input)
+    new(input).answer
   end
 
-  def self.get_result(input)
-    / == /.match("#{input}")
+  def get_result(input)
+    /== /.match("#{input}")
     $'
   end
 
-  def self.get_arguments(input)
-    / == /.match("#{input}")
-    $`
+  def get_arguments(input)
+    /==/.match("#{input}")
+    take_out_operators($`.split)
+  end
+
+  def take_out_operators(input)
+    input.find_all { |letter| letter != '+'}
+  end
+
+end
+
+class Solve
+  attr_reader :letters
+
+  def initialize(input)
+    @result = input[1]
+    @arguments = input[0]
+    @letters ||= all_letters(input)
+    @combos = test_combos(@letters)
+  end
+
+  def all_letters(input)
+    letters = Hash.new(0)
+    input.join.chars.each { |element| letters[element] = 0}
+    letters
+  end
+
+  def test_combos(letters)
+    return if (solve_puzzle(letters))
+    9.times do |i|
+      value = letters.select { |k,v| v < i }.keys.first
+      letters[value] += 1
+      test_combos(letters)
+    end
+  end
+
+  # def test_numbers(letter)
+  #   0..9.times do |i|
+  #     letters[letter] = i
+  #   end
+  # end
+
+  def solve_puzzle(letters)
+    return false if letters.reduce(0) { |sum,k| k[1] } == 0 
+    "#{@letters['I']}" + "#{@letters['B']}#{@letters['B']}" == "#{@letters['I']}#{@letters['L']}#{@letters['L']}"
   end
 end
