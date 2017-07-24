5971b15f01d6c1e0a07807069ecd41d3d44cfddd
Matt D <MDes41@users.noreply.github.com>
2017-07-24 17:59:50 -0600

    Rename var

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -4,22 +4,23 @@ class FlattenArray
   attr_reader :result
 
   def initialize
-    @result = []
+    @result     = []
   end
 
   def self.flatten(mixed_arr)
     new.flatten(mixed_arr)
   end
 
-  def flatten(mixed_arr)
-    mixed_arr.compact!
-    mixed_arr.shift if mixed_arr[0] == []
-    return result if mixed_arr == []
-    if mixed_arr[0].class == Fixnum
-      result << mixed_arr.shift
-      flatten(mixed_arr)
-    elsif mixed_arr[0].class == Array
-      flatten(mixed_arr.unshift(mixed_arr[0].shift))
+  def flatten(elements_to_process)
+    elements_to_process.compact!
+    current_element = elements_to_process[0]
+    elements_to_process.shift if current_element        == []
+    return result             if elements_to_process    == []
+    if current_element.class    == Fixnum
+      result << elements_to_process.shift
+      flatten(elements_to_process)
+    elsif current_element.class == Array
+      flatten(elements_to_process.unshift(current_element.shift))
     end
   end
 end
