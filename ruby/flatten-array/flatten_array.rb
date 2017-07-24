b43020712a539bf87258ad4cdf832c1c96757595
Matt D <MDes41@users.noreply.github.com>
2017-07-24 17:49:08 -0600

    Recursive solution

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -12,20 +12,14 @@ class FlattenArray
   end
 
   def flatten(mixed_arr)
+    mixed_arr.compact!
+    mixed_arr.shift if mixed_arr[0] == []
     return result if mixed_arr == []
     if mixed_arr[0].class == Fixnum
       result << mixed_arr.shift
       flatten(mixed_arr)
-    elsif mixed_arr[0] == nil
-      mixed_arr.shift
-      flatten(mixed_arr)
     elsif mixed_arr[0].class == Array
-      if mixed_arr[0] == []
-        mixed_arr.shift
-        flatten(mixed_arr)
-      else
-        flatten(mixed_arr.unshift(mixed_arr[0].shift))
-      end
+      flatten(mixed_arr.unshift(mixed_arr[0].shift))
     end
   end
 end
