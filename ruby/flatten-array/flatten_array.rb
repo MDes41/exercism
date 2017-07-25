33d35a02b269b908ef295170c018444592bc3200
Matt D <MDes41@users.noreply.github.com>
2017-07-24 19:00:06 -0600

    Refactored best solution

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,22 +1,18 @@
 class FlattenArray
   VERSION = 1
+  attr_reader :result
+
+  def initialize
+    @result = []
+  end
 
   def self.flatten(mixed_arr)
     new.flatten(mixed_arr)
   end
 
 
-  def flatten(array_to_flat)
-    result = []
-
-    array_to_flat.each do |item|
-      if item.is_a?(Array)
-        result += flatten(item)
-      else
-        result += [item]
-      end
-    end
-
+  def flatten(item)
+    item.is_a?(Array) ? item.each { |item| flatten(item) } : result.push(item)
     return result.compact
   end
 end
